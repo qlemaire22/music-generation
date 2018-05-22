f16a8b3e8b743ceb013cc118027294180e848e08
Justine Weber <justine.weber@epfl.ch>
2018-05-22 15:59:49 +0200

    Added .idea to gitignore

---
+++ vae/vae.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from keras.layers import Lambda, Input, Dense
+from keras.models import Model
+from keras.datasets import mnist
+from keras.losses import mse, binary_crossentropy
+from keras.utils import plot_model
+from keras import backend as K
+
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import os
+
