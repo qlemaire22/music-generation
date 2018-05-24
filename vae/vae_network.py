11925a94bb0f34cdac1afb223591fca035505003
Justine Weber <justine.weber@epfl.ch>
2018-05-24 17:40:31 +0200

    Separation done, not tested at all probably full of error.

--- vae/vae_network.py
+++ vae/vae_network.py
@@ -10,13 +10,6 @@ from keras.models import Model
 import vae_config
 
 class VAENetwork :
-    BATCH_SIZE = 32
-    ORIGINAL_DIM = 100
-    INTER_DIM = 50
-    LATENT_DIM = 4
-    EPSILON_STD = 1.0
-    NUMBER_EPOCHS = 100
-    NB_VECT = BATCH_SIZE * 22  # 704
 
     def __init__(self):
 
@@ -24,10 +17,10 @@ class VAENetwork :
         self.decoder_h = Dense(vae_config.INTER_DIM, activation='relu')
         self.decoder_mean = Dense(vae_config., activation='sigmoid')
 
-        x = Input(batch_shape=(batch_size, original_dim))
-        h = Dense(intermediate_dim, activation='relu')(x)
-        z_mean = Dense(latent_dim)(h)
-        z_log_sigma = Dense(latent_dim)(h)
+        x = Input(batch_shape=(vae_config.BATCH_SIZE, vae_config.ORIGINAL_DIM))
+        h = Dense(vae_config.INTER_DIM, activation='relu')(x)
+        z_mean = Dense(vae_config.LATENT_DIM)(h)
+        z_log_sigma = Dense(vae_config.LATENT_DIM)(h)
 
         # sample new similar points from the latent space
         z = Lambda(self.sampling)([z_mean, z_log_sigma])
@@ -39,45 +32,21 @@ class VAENetwork :
         self.encoder = Model(x, z_mean)
 
         self.model = Model(x, x_decoded_mean)
-        self.model.compile(optimizer='rmsprop', loss=self.vae_loss)
+        self.model.compile(optimizer='rmsprop', loss=self.vae_loss([z_mean, z_log_sigma]))
 
     # custom loss function : the sum of a reconstruction term, and the KL divergence regularization term
-    def vae_loss(x, x_decoded_mean):
+    def vae_loss(args, x, x_decoded_mean):
+        z_mean, z_log_sigma = args
         xent_loss = objectives.binary_crossentropy(x, x_decoded_mean)
         kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
         return xent_loss + kl_loss
 
     def sampling(args):
         z_mean, z_log_sigma = args
-        epsilon = K.random_normal(shape=(batch_size, latent_dim),
-                                  mean=0., stddev=epsilon_std)  # changed std to stddev because of error
+        epsilon = K.random_normal(shape=(vae_config.BATCH_SIZE, vae_config.LATENT_DIM),
+                                  mean=0., stddev=vae_config.EPSILON_STD)  # changed std to stddev because of error
         return z_mean + K.exp(z_log_sigma) * epsilon
 
-
-""""# encoder network, mapping inputs to our latent distribution parameters
-x = Input(batch_shape=(batch_size, original_dim))
-h = Dense(intermediate_dim, activation='relu')(x)
-z_mean = Dense(latent_dim)(h)
-z_log_sigma = Dense(latent_dim)(h)
-
-# sample new similar points from the latent space
-z = Lambda(sampling)([z_mean, z_log_sigma])
-
-# map the sampled latent points back to reconstructed inputs
-decoder_h = Dense(intermediate_dim, activation='relu')
-decoder_mean = Dense(original_dim, activation='sigmoid')
-h_decoded = decoder_h(z)
-x_decoded_mean = decoder_mean(h_decoded)
-
-# Models
-# end-to-end autoencoder
-vae = Model(x, x_decoded_mean)
-
-# encoder, from inputs to latent space
-encoder = Model(x, z_mean)
-
-# generator, from latent space to reconstructed inputs
-decoder_input = Input(shape=(latent_dim,))
-_h_decoded = decoder_h(decoder_input)
-_x_decoded_mean = decoder_mean(_h_decoded)
-generator = Model(decoder_input, _x_decoded_mean)"""
+    def load_weights(self, path):
+        self.model.load_weights(path)
+        print("Weights loaded:", path)
