3dfddea4ea3490859e8b56cd0ec89f5139e708b3
Quentin <quentin.lemaire@eyesnap.fr>
2018-05-27 01:59:51 +0200

    fix vae

--- vae/vae_config.py
+++ vae/vae_config.py
@@ -3,11 +3,8 @@ BATCH_SIZE = 50
 ORIGINAL_DIM = 512 * 6  # 3072
 LATENT_DIM = 4
 EPSILON_STD = 1.0
-NUMBER_EPOCHS = 100
-NB_VECT = BATCH_SIZE * 22  # 704
+NUMBER_EPOCHS = 10000
 
-# Dimension values for basic VAE
-INTER_DIM = 50
 
 # Dimension values for the deep VAE
 INTER_DIM1 = int(ORIGINAL_DIM / 4)  # 768
