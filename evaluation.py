c7355bbaf52c1c54e4ff98f35851cb165f96be72
Justine Weber <justine.weber@epfl.ch>
2018-05-30 01:06:07 +0200

    Evaluation function with histogram differences complete. Need to make the code cleaner.

--- evaluation.py
+++ evaluation.py
@@ -6,8 +6,13 @@ import os
 import data
 from tqdm import tqdm
 import copy
+import argparse
 
-SONG_PATH = "results/italia_output0.npy"
+SONG_PATH = "results/italia_output5.npy"
+HISTO_PATH = "histograms/"
+NB_SEMITONES = 12
+REAL_NB_NOTES = 52
+
 
 def evaluation():
     print("Evaluating: " + SONG_PATH)
@@ -19,13 +24,13 @@ def evaluation():
 
     _, _, n_vocab, pitchnames, _ = data.prepare_sequences()
 
-    #print(pitchnames)
+    # print(pitchnames)
 
     # create a dictionary to map pitches to integers
     note_to_int = dict((note, number)
                        for number, note in enumerate(pitchnames))
 
-    #print(note_to_int)
+    # print(note_to_int)
 
     song = np.load(SONG_PATH)
 
@@ -46,13 +51,14 @@ def evaluation():
     for i in range(10):
         print("Song position " + str(i) + ": " + filenames[minimums[i]])
 
-    #print("Closest song: " + filenames[np.argmin(distances)] + " distance: " + str(np.min(distances)))
+    # print("Closest song: " + filenames[np.argmin(distances)] + " distance: " + str(np.min(distances)))
 
-    #print(song)
+    # print(song)
     list_notes = list(np.load(filenames[np.argmin(distances)]))
     list_notes = [note_to_int[char] for char in list_notes]
-    #print(list_notes)
-    #print(distance_debug(song, list_notes))
+    # print(list_notes)
+    # print(distance_debug(song, list_notes))
+
 
 def distance(song1, song2):
     if len(song2) > len(song1):
@@ -71,16 +77,17 @@ def distance(song1, song2):
 
         if i2 < n1:
             temp = np.abs(song1[i1:i2] - song2)
-            #temp = np.where(temp > 0, 1, 0)
+            # temp = np.where(temp > 0, 1, 0)
             temp = np.sum(temp)
             distances.append(temp)
         else:
-            temp_song = np.append(song1[i1:n1-1], song1[:i2-n1+1])
+            temp_song = np.append(song1[i1:n1 - 1], song1[:i2 - n1 + 1])
             temp = np.abs(temp_song - song2)
-            #temp = np.where(temp > 0, 1, 0)
+            # temp = np.where(temp > 0, 1, 0)
             temp = np.sum(temp)
             distances.append(temp)
-    return min(distances)/n2
+    return min(distances) / n2
+
 
 def distance_debug(song1, song2):
     if len(song2) > len(song1):
@@ -110,7 +117,7 @@ def distance_debug(song1, song2):
                 print(np.abs(song1[i1:i2] - song2))
 
         else:
-            temp_song = np.append(song1[i1:n1-1], song1[:i2-n1+1])
+            temp_song = np.append(song1[i1:n1 - 1], song1[:i2 - n1 + 1])
             temp = np.ceil(np.abs(temp_song - song2))
             temp = np.where(temp > 0, 1, 0)
             temp = np.sum(temp)
@@ -124,13 +131,23 @@ def distance_debug(song1, song2):
     return min(distances)
 
 
-def evaluation2():
+def evaluation_whole():
     print("Evaluating: " + SONG_PATH)
+    files_path = "data/individual_songs/"
+
     filenames_temp = []
-    for filename in glob.glob("data/individual_songs/*"):
+    for filename in glob.glob(files_path + "*"):
         filenames_temp.append(filename)
 
     filenames = sorted(filenames_temp)
+
+    filenames_short = []
+    for filename in filenames:
+        if filename.startswith(files_path):
+            filenames_short.append(filename[len(files_path):])
+        else:
+            filenames_short.append(filename)
+    print(filenames_short[0])
 
     _, _, n_vocab, pitchnames, _ = data.prepare_sequences()
 
@@ -142,12 +159,13 @@ def evaluation2():
 
     distances = []
 
-    song_histogram = interval_histogram(song)
+    song_histogram = interval_histogram(pitch_histogram(song))
 
     for i in tqdm(range(len(filenames))):
         list_notes = list(np.load(filenames[i]))
         list_notes = [note_to_int[char] for char in list_notes]
-        current_histogram = interval_histogram(list_notes)
+        current_histogram = interval_histogram(pitch_histogram(list_notes))
+        np.save(HISTO_PATH + filenames_short[i], current_histogram)
         if len(list_notes) > 0:
             distances.append(compare_histograms(song_histogram, current_histogram))
         else:
@@ -158,7 +176,7 @@ def evaluation2():
     for i in range(10):
         print("Song position " + str(i) + ": " + filenames[minimums[i]])
 
-    # print("Closest song: " + filenames[np.argmin(distances)] + " distance: " + str(np.min(distances)))
+    print("Closest song: " + filenames[np.argmin(distances)] + " distance: " + str(np.min(distances)))
 
     # print(song)
     list_notes = list(np.load(filenames[np.argmin(distances)]))
@@ -167,42 +185,137 @@ def evaluation2():
     # print(distance_debug(song, list_notes))
 
 
+def evaluation_load():
+    print("Evaluating: " + SONG_PATH)
+    filenames_temp = []
+    files_path = "data/individual_songs/"
+    for filename in glob.glob(files_path + "*"):
+        filenames_temp.append(filename)
+
+    filenames = sorted(filenames_temp)
+
+    filenames_short = []
+    for filename in filenames:
+        if filename.startswith(files_path):
+            print(filename)
+            print(len(filename))
+            filenames_short.append(filename[len(files_path):])
+        else:
+            filenames_short.append(filename)
+    print(filenames_short[0])
+
+    _, _, n_vocab, pitchnames, _ = data.prepare_sequences()
+
+    # create a dictionary to map pitches to integers
+    note_to_int = dict((note, number)
+                       for number, note in enumerate(pitchnames))
+
+    song = np.load(SONG_PATH)
+
+    distances = []
+
+    song_histogram = interval_histogram(pitch_histogram(song))
+
+    for i in tqdm(range(len(filenames))):
+        path = HISTO_PATH + filenames_short[i]
+        current_histogram = np.load(path)
+        if current_histogram.size > 0:
+            distances.append(compare_histograms(song_histogram, current_histogram))
+        else:
+            distances.append(1000)
+
+    minimums = np.argsort(distances)
+
+    for i in range(10):
+        print("Song position " + str(i) + ": " + filenames[minimums[i]])
+
+    print("Closest song: " + filenames[np.argmin(distances)] + " distance: " + str(np.min(distances)))
+
+    # print(song)
+    list_notes = list(np.load(filenames[np.argmin(distances)]))
+    list_notes = [note_to_int[char] for char in list_notes]
+    # print(list_notes)
+    # print(distance_debug(song, list_notes))
+
+
+# inputs : two lists of length 12
+# output : sum of differences
 def compare_histograms(hist1, hist2):
+    result = 0
+    for i in range(NB_SEMITONES):
+        result += abs(hist1[i] - hist2[i])
+    return result
 
-    return 0
 
-
-def interval_histogram(song):
+def pitch_histogram(song):
     length = len(song)
     my_dict = note_to_mypitch_table()
-    histogram = [0] * config.REAL_NB_NOTES
+    histogram = [0] * REAL_NB_NOTES
     for i in range(length):
         histogram[my_dict[song[i]]] += 1
-    return histogram
+    return np.asarray(histogram)
+
+
+# input should be numpy array
+# return normalized list
+def interval_histogram(pitch_histo):
+    result = [0] * NB_SEMITONES
+    length = pitch_histo.size
+    max_idx = np.argmax(pitch_histo)
+
+    # count + normalize frequency of each note
+    # as a relative to the most recurrent note
+    for idx in range(length):
+        result[int((idx - max_idx) % NB_SEMITONES)] += pitch_histo[idx] / length
+
+    return result
 
 
 def note_to_mypitch_table():
     _, _, _, pitchnames, _ = data.prepare_sequences()
-    
+
     # create a dictionary to map pitches to integers
     note_to_int = dict((note, number)
                        for number, note in enumerate(pitchnames))
 
     ordered_pitch = {'A2': 0, 'B-2': 1, 'B2': 2, 'C3': 3, 'C#3': 4,
-            'D3': 5, 'D#3': 6, 'E-3': 6, 'E3': 7, 'F3': 8, 'F#3': 9, 'G3': 10, 'G#3': 11,
-            'A-3': 11, 'A3': 12, 'A#3': 13, 'B-3': 13, 'B3': 14, 'C-4': 14, 'B#3': 15, 'C4': 15, 'C#4': 16,
-            'D-4': 16, 'D4': 17, 'D#4': 18, 'E-4': 18, 'E4': 19, 'F-4': 19, 'E#4': 20, 'F4': 20, 'F#4': 21,
-            'G-4': 21, 'G4': 22, 'G#4': 23, 'A-4': 23, 'A4': 24, 'A#4': 25, 'B-4': 25, 'B4': 26, 'C-5': 26,
-            'B#4': 27, 'C5': 27, 'C#5': 28, 'D-5': 28, 'D5': 29, 'D#5': 30, 'E-5': 30, 'E5': 31, 'F-5': 31,
-            'E#5': 32, 'F5': 32, 'F#5': 33, 'G-5': 33, 'G5': 34, 'G#5': 35, 'A-5': 35, 'A5': 36, 'A#5': 37,
-            'B-5': 37, 'B5': 38, 'B#5': 39, 'C6': 39, 'C#6': 40, 'D-6': 40, 'D6': 41, 'D#6': 42, 'E-6': 42,
-            'E6': 43, 'F6': 44, 'F#6': 45, 'G6': 46, 'G#6': 47, 'A6': 48, 'B-6': 49, 'B6': 50, 'C7': 51}
+                     'D3': 5, 'D#3': 6, 'E-3': 6, 'E3': 7, 'F3': 8, 'F#3': 9, 'G3': 10, 'G#3': 11,
+                     'A-3': 11, 'A3': 12, 'A#3': 13, 'B-3': 13, 'B3': 14, 'C-4': 14, 'B#3': 15, 'C4': 15, 'C#4': 16,
+                     'D-4': 16, 'D4': 17, 'D#4': 18, 'E-4': 18, 'E4': 19, 'F-4': 19, 'E#4': 20, 'F4': 20, 'F#4': 21,
+                     'G-4': 21, 'G4': 22, 'G#4': 23, 'A-4': 23, 'A4': 24, 'A#4': 25, 'B-4': 25, 'B4': 26, 'C-5': 26,
+                     'B#4': 27, 'C5': 27, 'C#5': 28, 'D-5': 28, 'D5': 29, 'D#5': 30, 'E-5': 30, 'E5': 31, 'F-5': 31,
+                     'E#5': 32, 'F5': 32, 'F#5': 33, 'G-5': 33, 'G5': 34, 'G#5': 35, 'A-5': 35, 'A5': 36, 'A#5': 37,
+                     'B-5': 37, 'B5': 38, 'B#5': 39, 'C6': 39, 'C#6': 40, 'D-6': 40, 'D6': 41, 'D#6': 42, 'E-6': 42,
+                     'E6': 43, 'F6': 44, 'F#6': 45, 'G6': 46, 'G#6': 47, 'A6': 48, 'B-6': 49, 'B6': 50, 'C7': 51}
 
     result = {}
     for note in pitchnames:
-        result[note_to_int[note]]= ordered_pitch[note]
+        result[note_to_int[note]] = ordered_pitch[note]
 
     return result
 
+
 if __name__ == '__main__':
-    evaluation()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--whole', default=0,
+                        help="0 if you want to recompute the histograms for the whole dataset,"
+                             "1 if you want to load pre-existing histograms.", type=int)
+
+    args = parser.parse_args()
+
+    var = args.whole
+
+    if var == 0:
+        if not os.path.exists("histograms"):
+            os.makedirs("histograms")
+        evaluation_whole()
+    elif var == 1:
+        if not (os.path.exists("histograms")):
+            print("There is histograms, under histogram directory. Please try computing "
+                  "all the histograms first (parameter 1).")
+        else:
+            evaluation_load()
+    else:
+        print("Wrong parameter for whole. Please enter 1 for computing the histogram of the whole dataset,"
+              "or 0 for using preexisting data (if any)")
